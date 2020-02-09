e8dedcc4efb86d6857044668416a3f00cc35c6e8
KinDark <dkinicki@wp.pl>
2020-02-09 10:49:48 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Ta linia i poniższa jest fkmentarzem
+		// Wypisz "Hello Word" do okna teminala.
+		System.out.println("hello, World");
+		}
+	}
